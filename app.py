87c4629c45dd980cfdf25b4c121bcff2980e0d1a
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-14 20:22:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,56 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from sympy import symbols, integrate, latex
+import numpy as np
+import matplotlib.pyplot as plt
 
-"""
-# Welcome to Streamlit!
+# Define the functions
+x = symbols('x')
+functions = {
+    'Function 1': x**2,
+    'Function 2': 2*x**3-7*x+4,
+    'Function 3': x**4
+}
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def main():
+    st.title("Integral Solver")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    # Create a container to center-align the widgets
+    container = st.beta_container()
+    col1, col2, col3 = container.beta_columns(3)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    # Function selection
+    with col1:
+        selected_function = st.selectbox("Select a function", list(functions.keys()))
 
+    # Bounds selection
+    with col2:
+        left_bound = st.number_input("Enter the left bound", value=0.0)
+    with col3:
+        right_bound = st.number_input("Enter the right bound", value=1.0)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    # Steps selection
+    steps = st.number_input("Enter the number of steps", value=100)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    # Calculate the integral
+    integral = integrate(functions[selected_function], (x, left_bound, right_bound))
 
-    points_per_turn = total_points / num_turns
+    # Display the LaTeX name of the function
+    st.latex(f"Selected function: {latex(functions[selected_function])}")
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    # Plot the function with the integral area
+    x_vals = np.linspace(left_bound, right_bound, 100)
+    y_vals = [functions[selected_function].subs(x, val) for val in x_vals]
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    plt.plot(x_vals, y_vals, label=selected_function)
+    plt.fill_between(x_vals, y_vals, 0, where=(x_vals >= left_bound) & (x_vals <= right_bound), alpha=0.3)
+
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    st.pyplot()
+
+    # Display the result
+    st.write(f"The integral of {selected_function} from {left_bound} to {right_bound} is: {integral}")
+
+if __name__ == "__main__":
+    main()
