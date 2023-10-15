1399e64c069c664c186e5d23b2a0ecb12e47136f
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-15 19:43:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ functions = {
 }
 
 def main():
-    st.title("Интегрируем v325.alpha")
+    st.title("Интегрируем v0.325.alpha")
 
     # Create a container to center-align the widgets
     container = st.container()
