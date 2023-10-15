c539a64253c9295fd1a0dce5e0b6de08d0f4786d
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-15 18:23:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -69,9 +69,14 @@ def main():
     # Plot the function
     fig, ax = plt.subplots()
     ax.plot(x_vals, y_vals, label=f"f(x) = {latex(functions[selected_function])}")
+
     x_vals = np.linspace(left_bound, right_bound, 100)
     y_vals = [functions[selected_function].subs(x, val) for val in x_vals]
-    y_vals = np.where(np.isnan(y_vals), np.nan, y_vals)
+
+    # Handle non-finite values
+    y_vals = np.array(y_vals)
+    y_vals[~np.isfinite(y_vals)] = 0.0
+
     ax.fill_between(x_vals, y_vals, where=(x_vals >= left_bound) & (x_vals <= right_bound), alpha=0.3)
     ax.legend()
     ax.set_xlabel("x")
