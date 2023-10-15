4509e0ee5241c209eb741a5ad1e534fda907cea8
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-15 18:39:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -71,8 +71,8 @@ def main():
     # Plot the function
     fig, ax = plt.subplots()
     ax.plot(x_vals, y_vals, label=f"f(x) = {latex(functions[selected_function])}")
-    ax.axvline(left_bound, color='red', linestyle='--', label='a')
-    ax.axvline(right_bound, color='green', linestyle='--', label='b')
+    ax.axvline(left_bound, color='red', linestyle='--', label=f'a={left_bound}')
+    ax.axvline(right_bound, color='green', linestyle='--', label=f'b={right_bound}')
     ax.legend()
     ax.set_xlabel("x")
     ax.set_ylabel("f(x)")
