1d222d4f62edbd3b89179e17ae5356c850559c7f
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-15 18:15:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -69,6 +69,8 @@ def main():
     # Plot the function
     fig, ax = plt.subplots()
     ax.plot(x_vals, y_vals, label=f"f(x) = {latex(functions[selected_function])}")
+    print(x_vals)
+    print(y_vals)
     ax.fill_between(x_vals, y_vals, where=(x_vals >= left_bound) & (x_vals <= right_bound), alpha=0.3)
     ax.legend()
     ax.set_xlabel("x")
