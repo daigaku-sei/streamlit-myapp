c93b788a5aba10e4b63b6a874c7a83e82b0ea92f
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-22 18:31:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sympy import symbols, sympify, integrate, latex, exp, sin, cos, pi
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 st.set_option('deprecation.showPyplotGlobalUse', False)
 # Define the functions
 x = symbols('x')
