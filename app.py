5939d124ec1c3bf4b10a1d1b1aefafee82656cdf
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-26 19:22:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,7 @@ functions = {
 def main():
     hide_menu = """
     <style>
-    #GithubIcon {
-      visibility: hidden;
-    }
+    
     #MainMenu {
         visibility: hidden;
     }
@@ -36,17 +34,12 @@ def main():
     </style>
     """
     st.markdown(hide_menu, unsafe_allow_html=True)
-    st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True)
+    hide_github_icon = “”"
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+    
+    “”"
+    st.markdown(hide_github_icon, unsafe_allow_html=True)
+        
     st.title("Интегрируем")
 
     # Create a container to center-align the widgets
